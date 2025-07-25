dcef83b51f918b55b27370a477767ef6097e8cb6
Hoang Dinh <minhhoang.1105@gmail.com>
2025-07-25 10:01:31 +0700

    Enhance UIView+ChameleonPrivate.m with additional UIKit imports for improved functionality

--- Sources/Chameleon/ObjC/UIView+ChameleonPrivate.m
+++ Sources/Chameleon/ObjC/UIView+ChameleonPrivate.m
@@ -6,6 +6,11 @@
 //  Copyright (c) 2015 Vicc Alexander. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIView.h>
+#import <UIKit/UIWindow.h>
 #import "UIView+ChameleonPrivate.h"
 
 @implementation UIView (ChameleonPrivate)
